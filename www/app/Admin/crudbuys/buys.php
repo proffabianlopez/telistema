76c4dc70d8f0db4313cd4d709bf31363a1e8db15
christian.pereyra_ <new_chriss@hotmail.com.ar>
2024-06-19 21:12:22 -0300

    correction view

--- www/app/Admin/crudbuys/buys.php
+++ www/app/Admin/crudbuys/buys.php
@@ -53,172 +53,173 @@ include('../../Querys/querys.php');
                 </div>
                 <div class="col-lg-2"></div>
             </div>
+
             <div class="wrapper wrapper-content animated fadeInRight ecommerce">
 
 
-    <div class="ibox-content m-b-sm border-bottom">
-        <div class="row">
-            <div class="col-sm-4">
-                <div class="form-group">
-                    <label class="control-label" for="order_id">Order ID</label>
-                    <input type="text" id="order_id" name="order_id" value="" placeholder="Order ID" class="form-control">
-                </div>
-            </div>
-            <div class="col-sm-4">
-                <div class="form-group">
-                    <label class="control-label" for="status">Order status</label>
-                    <input type="text" id="status" name="status" value="" placeholder="Status" class="form-control">
-                </div>
-            </div>
-            <div class="col-sm-4">
-                <div class="form-group">
-                    <label class="control-label" for="customer">Customer</label>
-                    <input type="text" id="customer" name="customer" value="" placeholder="Customer" class="form-control">
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-sm-4">
-                <div class="form-group">
-                    <label class="control-label" for="date_added">Date added</label>
-                    <div class="input-group date">
-                        <span class="input-group-addon"><i class="fa fa-calendar"></i></span><input id="date_added" type="text" class="form-control" value="03/04/2014">
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-4">
-                <div class="form-group">
-                    <label class="control-label" for="date_modified">Date modified</label>
-                    <div class="input-group date">
-                        <span class="input-group-addon"><i class="fa fa-calendar"></i></span><input id="date_modified" type="text" class="form-control" value="03/06/2014">
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-4">
-                <div class="form-group">
-                    <label class="control-label" for="amount">Amount</label>
-                    <input type="text" id="amount" name="amount" value="" placeholder="Amount" class="form-control">
-                </div>
-            </div>
-        </div>
-
-    </div>
-
+<div class="ibox-content m-b-sm border-bottom">
     <div class="row">
-        <div class="col-lg-12">
-            <div class="ibox">
-                <div class="ibox-content">
-
-                    <table class="footable table table-stripped toggle-arrow-tiny footable-loaded phone breakpoint" data-page-size="15">
-                        <thead>
-                            <tr>
-
-                                <th class="footable-visible footable-sortable footable-first-column">Order ID<span class="footable-sort-indicator"></span></th>
-                                <th data-hide="phone" class="footable-sortable" style="display: none;">Customer<span class="footable-sort-indicator"></span></th>
-                                <th data-hide="phone" class="footable-sortable" style="display: none;">Amount<span class="footable-sort-indicator"></span></th>
-                                <th data-hide="phone" class="footable-sortable" style="display: none;">Date added<span class="footable-sort-indicator"></span></th>
-                                <th data-hide="phone,tablet" class="footable-sortable" style="display: none;">Date modified<span class="footable-sort-indicator"></span></th>
-                                <th data-hide="phone" class="footable-sortable" style="display: none;">Status<span class="footable-sort-indicator"></span></th>
-                                <th class="text-right footable-visible footable-sortable footable-last-column">Action<span class="footable-sort-indicator"></span></th>
-
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr class="footable-even" style="display: table-row;">
-                                <td class="footable-visible footable-first-column"><span class="footable-toggle"></span>
-                                    3214
-                                </td>
-                                <td class="" style="display: none;">
-                                    Customer example
-                                </td>
-                                <td class="" style="display: none;">
-                                    $500.00
-                                </td>
-                                <td class="" style="display: none;">
-                                    03/04/2015
-                                </td>
-                                <td class="" style="display: none;">
-                                    03/05/2015
-                                </td>
-                                <td class="" style="display: none;">
-                                    <span class="label label-primary">Pending</span>
-                                </td>
-                                <td class="text-right footable-visible footable-last-column">
-                                    <div class="btn-group">
-                                        <button class="btn-white btn btn-xs">View</button>
-                                        <button class="btn-white btn btn-xs">Edit</button>
-                                        <button class="btn-white btn btn-xs">Delete</button>
-                                    </div>
-                                </td>
-                            </tr>
-                            <tr class="footable-even" style="display: table-row;">
-                                <td class="footable-visible footable-first-column"><span class="footable-toggle"></span>
-                                    642
-                                </td>
-                                <td class="" style="display: none;">
-                                    Customer example
-                                </td>
-                                <td class="" style="display: none;">
-                                    $6843.00
-                                </td>
-                                <td class="" style="display: none;">
-                                    10/04/2015
-                                </td>
-                                <td class="" style="display: none;">
-                                    13/07/2015
-                                </td>
-                                <td class="" style="display: none;">
-                                    <span class="label label-success">Shipped</span>
-                                </td>
-                                <td class="text-right footable-visible footable-last-column">
-                                    <div class="btn-group">
-                                        <button class="btn-white btn btn-xs">View</button>
-                                        <button class="btn-white btn btn-xs">Edit</button>
-                                        <button class="btn-white btn btn-xs">Delete</button>
-                                    </div>
-                                </td>
-                            </tr>
-                            <tr class="footable-even" style="display: table-row;">
-                                <td class="footable-visible footable-first-column"><span class="footable-toggle"></span>
-                                    324
-                                </td>
-                                <td class="" style="display: none;">
-                                    Customer example
-                                </td>
-                                <td class="" style="display: none;">
-                                    $320.00
-                                </td>
-                                <td class="" style="display: none;">
-                                    12/04/2015
-                                </td>
-                                <td class="" style="display: none;">
-                                    21/07/2015
-                                </td>
-                                <td class="" style="display: none;">
-                                    <span class="label label-warning">Expired</span>
-                                </td>
-                                <td class="text-right footable-visible footable-last-column">
-                                    <div class="btn-group">
-                                        <button class="btn-white btn btn-xs">View</button>
-                                        <button class="btn-white btn btn-xs">Edit</button>
-                                        <button class="btn-white btn btn-xs">Delete</button>
-                                    </div>
-                                </td>
-                            </tr>
-                        </tbody>
-                        <tfoot>
-                            <tr>
-                                <td colspan="7" class="footable-visible">
-                                    <ul class="pagination pull-right"><li class="footable-page-arrow disabled"><a data-page="first" href="#first">«</a></li><li class="footable-page-arrow disabled"><a data-page="prev" href="#prev">‹</a></li><li class="footable-page active"><a data-page="0" href="#">1</a></li><li class="footable-page"><a data-page="1" href="#">2</a></li><li class="footable-page"><a data-page="2" href="#">3</a></li><li class="footable-page-arrow"><a data-page="next" href="#next">›</a></li><li class="footable-page-arrow"><a data-page="last" href="#last">»</a></li></ul>
-                                </td>
-                            </tr>
-                        </tfoot>
-                    </table>
-
-                </div>
+        <div class="col-sm-4">
+            <div class="form-group">
+                <label class="control-label" for="order_id">Order ID</label>
+                <input type="text" id="order_id" name="order_id" value="" placeholder="Order ID" class="form-control">
+            </div>
+        </div>
+        <div class="col-sm-4">
+            <div class="form-group">
+                <label class="control-label" for="status">Order status</label>
+                <input type="text" id="status" name="status" value="" placeholder="Status" class="form-control">
+            </div>
+        </div>
+        <div class="col-sm-4">
+            <div class="form-group">
+                <label class="control-label" for="customer">Customer</label>
+                <input type="text" id="customer" name="customer" value="" placeholder="Customer" class="form-control">
             </div>
         </div>
     </div>
+    <div class="row">
+        <div class="col-sm-4">
+            <div class="form-group">
+                <label class="control-label" for="date_added">Date added</label>
+                <div class="input-group date">
+                    <span class="input-group-addon"><i class="fa fa-calendar"></i></span><input id="date_added" type="text" class="form-control" value="03/04/2014">
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4">
+            <div class="form-group">
+                <label class="control-label" for="date_modified">Date modified</label>
+                <div class="input-group date">
+                    <span class="input-group-addon"><i class="fa fa-calendar"></i></span><input id="date_modified" type="text" class="form-control" value="03/06/2014">
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4">
+            <div class="form-group">
+                <label class="control-label" for="amount">Amount</label>
+                <input type="text" id="amount" name="amount" value="" placeholder="Amount" class="form-control">
+            </div>
+        </div>
+    </div>
+
+</div>
+
+<div class="row">
+    <div class="col-lg-12">
+        <div class="ibox">
+            <div class="ibox-content">
+
+                <table class="footable table table-stripped toggle-arrow-tiny footable-loaded default" data-page-size="15">
+                    <thead>
+                        <tr>
+
+                            <th class="footable-visible footable-sortable footable-first-column">Order ID<span class="footable-sort-indicator"></span></th>
+                            <th data-hide="phone" class="footable-sortable footable-visible" style="display: table-cell;">Customer<span class="footable-sort-indicator"></span></th>
+                            <th data-hide="phone" class="footable-sortable footable-visible" style="display: table-cell;">Amount<span class="footable-sort-indicator"></span></th>
+                            <th data-hide="phone" class="footable-sortable footable-visible" style="display: table-cell;">Date added<span class="footable-sort-indicator"></span></th>
+                            <th data-hide="phone,tablet" class="footable-sortable footable-visible" style="display: table-cell;">Date modified<span class="footable-sort-indicator"></span></th>
+                            <th data-hide="phone" class="footable-sortable footable-visible" style="display: table-cell;">Status<span class="footable-sort-indicator"></span></th>
+                            <th class="text-right footable-visible footable-sortable footable-last-column">Action<span class="footable-sort-indicator"></span></th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="footable-even" style="display: table-row;">
+                            <td class="footable-visible footable-first-column"><span class="footable-toggle"></span>
+                                3214
+                            </td>
+                            <td class="footable-visible" style="display: table-cell;">
+                                Customer example
+                            </td>
+                            <td class="footable-visible" style="display: table-cell;">
+                                $500.00
+                            </td>
+                            <td class="footable-visible" style="display: table-cell;">
+                                03/04/2015
+                            </td>
+                            <td class="footable-visible" style="display: table-cell;">
+                                03/05/2015
+                            </td>
+                            <td class="footable-visible" style="display: table-cell;">
+                                <span class="label label-primary">Pending</span>
+                            </td>
+                            <td class="text-right footable-visible footable-last-column">
+                                <div class="btn-group">
+                                    <button class="btn-white btn btn-xs">View</button>
+                                    <button class="btn-white btn btn-xs">Edit</button>
+                                    <button class="btn-white btn btn-xs">Delete</button>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr class="footable-even" style="display: table-row;">
+                            <td class="footable-visible footable-first-column"><span class="footable-toggle"></span>
+                                642
+                            </td>
+                            <td class="footable-visible" style="display: table-cell;">
+                                Customer example
+                            </td>
+                            <td class="footable-visible" style="display: table-cell;">
+                                $6843.00
+                            </td>
+                            <td class="footable-visible" style="display: table-cell;">
+                                10/04/2015
+                            </td>
+                            <td class="footable-visible" style="display: table-cell;">
+                                13/07/2015
+                            </td>
+                            <td class="footable-visible" style="display: table-cell;">
+                                <span class="label label-success">Shipped</span>
+                            </td>
+                            <td class="text-right footable-visible footable-last-column">
+                                <div class="btn-group">
+                                    <button class="btn-white btn btn-xs">View</button>
+                                    <button class="btn-white btn btn-xs">Edit</button>
+                                    <button class="btn-white btn btn-xs">Delete</button>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr class="footable-even" style="display: table-row;">
+                            <td class="footable-visible footable-first-column"><span class="footable-toggle"></span>
+                                324
+                            </td>
+                            <td class="footable-visible" style="display: table-cell;">
+                                Customer example
+                            </td>
+                            <td class="footable-visible" style="display: table-cell;">
+                                $320.00
+                            </td>
+                            <td class="footable-visible" style="display: table-cell;">
+                                12/04/2015
+                            </td>
+                            <td class="footable-visible" style="display: table-cell;">
+                                21/07/2015
+                            </td>
+                            <td class="footable-visible" style="display: table-cell;">
+                                <span class="label label-warning">Expired</span>
+                            </td>
+                            <td class="text-right footable-visible footable-last-column">
+                                <div class="btn-group">
+                                    <button class="btn-white btn btn-xs">View</button>
+                                    <button class="btn-white btn btn-xs">Edit</button>
+                                    <button class="btn-white btn btn-xs">Delete</button>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <td colspan="7" class="footable-visible">
+                                <ul class="pagination pull-right"><li class="footable-page-arrow disabled"><a data-page="first" href="#first">«</a></li><li class="footable-page-arrow disabled"><a data-page="prev" href="#prev">‹</a></li><li class="footable-page active"><a data-page="0" href="#">1</a></li><li class="footable-page"><a data-page="1" href="#">2</a></li><li class="footable-page"><a data-page="2" href="#">3</a></li><li class="footable-page-arrow"><a data-page="next" href="#next">›</a></li><li class="footable-page-arrow"><a data-page="last" href="#last">»</a></li></ul>
+                            </td>
+                        </tr>
+                    </tfoot>
+                </table>
+
+            </div>
+        </div>
+    </div>
+</div>
 
 
 </div>
