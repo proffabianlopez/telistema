1b69bab2ad5a6a8c7e3443ced3244c3ac1a1b9b6
fabain-hasiaver-1997 <adanhasinaver1997@gmail.com>
2024-06-21 19:36:45 -0300

    Dashboard de prueba, no mergear

--- www/app/Admin/crudadmins/dashboard.php
+++ www/app/Admin/crudadmins/dashboard.php
@@ -71,77 +71,269 @@ include('../../dbConnection.php');
 
           </div>
         </div>
-        <div class="col-lg-6">
-          <div>
-            <table class="table">
-              <tbody>
-                <tr>
-                  <td>
-                    <button type="button" class="btn btn-danger m-r-sm">12</button>
-                    Total messages
-                  </td>
-                  <td>
-                    <button type="button" class="btn btn-primary m-r-sm">28</button>
-                    Posts
-                  </td>
-                  <td>
-                    <button type="button" class="btn btn-info m-r-sm">15</button>
-                    Comments
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <button type="button" class="btn btn-info m-r-sm">20</button>
-                    News
-                  </td>
-                  <td>
-                    <button type="button" class="btn btn-success m-r-sm">40</button>
-                    Likes
-                  </td>
-                  <td>
-                    <button type="button" class="btn btn-danger m-r-sm">30</button>
-                    Notifications
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <button type="button" class="btn btn-warning m-r-sm">20</button>
-                    Albums
-                  </td>
-                  <td>
-                    <button type="button" class="btn btn-default m-r-sm">40</button>
-                    Groups
-                  </td>
-                  <td>
-                    <button type="button" class="btn btn-warning m-r-sm">30</button>
-                    Permissions
-                  </td>
-                </tr>
-              </tbody>
-            </table>
+      </div>
+      <div class="wrapper wrapper-content animated fadeInRight">
+        <div class="row">
+          <div class="col-lg-4">
+            <div class="widget style1 lazur-bg">
+              <div class="row">
+                <div class="col-xs-4 text-center">
+                  <i class="fa fa-bell fa-5x"></i>
+                </div>
+                <div class="col-xs-8 text-right">
+                  <span>Ordenes Pendientes</span>
+                  <h2 class="font-bold">5</h2>
+                </div>
+              </div>
+            </div>
           </div>
-
+          <div class="col-lg-4">
+            <div class="widget style1 blue-bg">
+              <div class="row">
+                <div class="col-xs-4">
+                  <i class="fa fa-check-square fa-5x"></i>
+                </div>
+                <div class="col-xs-8 text-right">
+                  <span>Ordenes Completadas</span>
+                  <h2 class="font-bold">4</h2>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4">
+            <div class="widget style1 red-bg ">
+              <div class="row">
+                <div class="col-xs-4">
+                  <i class="fa fa-exclamation-triangle fa-5x"></i>
+                </div>
+                <div class="col-xs-8 text-right">
+                  <span>Ordenes a completar</span>
+                  <h2 class="font-bold">260</h2>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
 
+      <div class="wrapper wrapper-content  animated fadeInRight">
+        <div class="row">
+          <div class="col-lg-4">
+            <div class="ibox">
+              <div class="ibox-content">
+                <h3>Ordenes Pendientes</h3>
+                <p class="small"><i class="fa fa-hand-o-up"></i> Drag task between list</p>
 
+                <div class="input-group">
+                  <input type="text" placeholder="Add new task. " class="input input-sm form-control">
+                  <span class="input-group-btn">
+                    <button type="button" class="btn btn-sm btn-white"> <i class="fa fa-plus"></i> Add task</button>
+                  </span>
+                </div>
 
-    </div>
-    <div class="footer">
-      <div class="pull-right">
-      </div>
-      <div>
-        <strong>Copyright</strong> Telistema &copy; 2024
+                <ul class="sortable-list connectList agile-list" id="todo">
+                  <li class="warning-element" id="task1">
+                    Simply dummy text of the printing and typesetting industry.
+                    <div class="agile-detail">
+                      <a href="#" class="pull-right btn btn-xs btn-white">Tag</a>
+                      <i class="fa fa-clock-o"></i> 12.10.2015
+                    </div>
+                  </li>
+                  <li class="success-element" id="task2">
+                    Many desktop publishing packages and web page editors now use Lorem Ipsum as their default.
+                    <div class="agile-detail">
+                      <a href="#" class="pull-right btn btn-xs btn-white">Mark</a>
+                      <i class="fa fa-clock-o"></i> 05.04.2015
+                    </div>
+                  </li>
+                  <li class="info-element" id="task3">
+                    Sometimes by accident, sometimes on purpose (injected humour and the like).
+                    <div class="agile-detail">
+                      <a href="#" class="pull-right btn btn-xs btn-white">Mark</a>
+                      <i class="fa fa-clock-o"></i> 16.11.2015
+                    </div>
+                  </li>
+                  <li class="danger-element" id="task4">
+                    All the Lorem Ipsum generators
+                    <div class="agile-detail">
+                      <a href="#" class="pull-right btn btn-xs btn-primary">Done</a>
+                      <i class="fa fa-clock-o"></i> 06.10.2015
+                    </div>
+                  </li>
+                  <li class="warning-element" id="task5">
+                    Which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
+                    <div class="agile-detail">
+                      <a href="#" class="pull-right btn btn-xs btn-white">Tag</a>
+                      <i class="fa fa-clock-o"></i> 09.12.2015
+                    </div>
+                  </li>
+                  <li class="warning-element" id="task6">
+                    Packages and web page editors now use Lorem Ipsum as
+                    <div class="agile-detail">
+                      <a href="#" class="pull-right btn btn-xs btn-primary">Done</a>
+                      <i class="fa fa-clock-o"></i> 08.04.2015
+                    </div>
+                  </li>
+                  <li class="success-element" id="task7">
+                    Many desktop publishing packages and web page editors now use Lorem Ipsum as their default.
+                    <div class="agile-detail">
+                      <a href="#" class="pull-right btn btn-xs btn-white">Mark</a>
+                      <i class="fa fa-clock-o"></i> 05.04.2015
+                    </div>
+                  </li>
+                  <li class="info-element" id="task8">
+                    Sometimes by accident, sometimes on purpose (injected humour and the like).
+                    <div class="agile-detail">
+                      <a href="#" class="pull-right btn btn-xs btn-white">Mark</a>
+                      <i class="fa fa-clock-o"></i> 16.11.2015
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4">
+            <div class="ibox">
+              <div class="ibox-content">
+                <h3>Ordenes a completar</h3>
+                <p class="small"><i class="fa fa-hand-o-up"></i> Drag task between list</p>
+                <ul class="sortable-list connectList agile-list" id="inprogress">
+                  <li class="success-element" id="task9">
+                    Quisque venenatis ante in porta suscipit.
+                    <div class="agile-detail">
+                      <a href="#" class="pull-right btn btn-xs btn-white">Tag</a>
+                      <i class="fa fa-clock-o"></i> 12.10.2015
+                    </div>
+                  </li>
+                  <li class="success-element" id="task10">
+                    Phasellus sit amet tortor sed enim mollis accumsan in consequat orci.
+                    <div class="agile-detail">
+                      <a href="#" class="pull-right btn btn-xs btn-white">Mark</a>
+                      <i class="fa fa-clock-o"></i> 05.04.2015
+                    </div>
+                  </li>
+                  <li class="warning-element" id="task11">
+                    Nunc sed arcu at ligula faucibus tempus ac id felis. Vestibulum et nulla quis turpis sagittis fringilla.
+                    <div class="agile-detail">
+                      <a href="#" class="pull-right btn btn-xs btn-white">Mark</a>
+                      <i class="fa fa-clock-o"></i> 16.11.2015
+                    </div>
+                  </li>
+                  <li class="warning-element" id="task12">
+                    Ut porttitor augue non sapien mollis accumsan.
+                    Nulla non elit eget lacus elementum viverra.
+                    <div class="agile-detail">
+                      <a href="#" class="pull-right btn btn-xs btn-white">Tag</a>
+                      <i class="fa fa-clock-o"></i> 09.12.2015
+                    </div>
+                  </li>
+                  <li class="info-element" id="task13">
+                    Packages and web page editors now use Lorem Ipsum as
+                    <div class="agile-detail">
+                      <a href="#" class="pull-right btn btn-xs btn-primary">Done</a>
+                      <i class="fa fa-clock-o"></i> 08.04.2015
+                    </div>
+                  </li>
+                  <li class="success-element" id="task14">
+                    Quisque lacinia tellus et odio ornare maximus.
+                    <div class="agile-detail">
+                      <a href="#" class="pull-right btn btn-xs btn-white">Mark</a>
+                      <i class="fa fa-clock-o"></i> 05.04.2015
+                    </div>
+                  </li>
+                  <li class="danger-element" id="task15">
+                    Enim mollis accumsan in consequat orci.
+                    <div class="agile-detail">
+                      <a href="#" class="pull-right btn btn-xs btn-white">Mark</a>
+                      <i class="fa fa-clock-o"></i> 11.04.2015
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4">
+            <div class="ibox">
+              <div class="ibox-content">
+                <h3>Ordenes Completadas</h3>
+                <p class="small"><i class="fa fa-hand-o-up"></i> Drag task between list</p>
+                <ul class="sortable-list connectList agile-list" id="completed">
+                  <li class="info-element" id="task16">
+                    Sometimes by accident, sometimes on purpose (injected humour and the like).
+                    <div class="agile-detail">
+                      <a href="#" class="pull-right btn btn-xs btn-white">Mark</a>
+                      <i class="fa fa-clock-o"></i> 16.11.2015
+                    </div>
+                  </li>
+                  <li class="warning-element" id="task17">
+                    Ut porttitor augue non sapien mollis accumsan.
+                    Nulla non elit eget lacus elementum viverra.
+                    <div class="agile-detail">
+                      <a href="#" class="pull-right btn btn-xs btn-white">Tag</a>
+                      <i class="fa fa-clock-o"></i> 09.12.2015
+                    </div>
+                  </li>
+                  <li class="warning-element" id="task18">
+                    Which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
+                    <div class="agile-detail">
+                      <a href="#" class="pull-right btn btn-xs btn-white">Tag</a>
+                      <i class="fa fa-clock-o"></i> 09.12.2015
+                    </div>
+                  </li>
+                  <li class="warning-element" id="task19">
+                    Packages and web page editors now use Lorem Ipsum as
+                    <div class="agile-detail">
+                      <a href="#" class="pull-right btn btn-xs btn-primary">Done</a>
+                      <i class="fa fa-clock-o"></i> 08.04.2015
+                    </div>
+                  </li>
+                  <li class="success-element" id="task20">
+                    Many desktop publishing packages and web page editors now use Lorem Ipsum as their default.
+                    <div class="agile-detail">
+                      <a href="#" class="pull-right btn btn-xs btn-white">Mark</a>
+                      <i class="fa fa-clock-o"></i> 05.04.2015
+                    </div>
+                  </li>
+                  <li class="info-element" id="task21">
+                    Sometimes by accident, sometimes on purpose (injected humour and the like).
+                    <div class="agile-detail">
+                      <a href="#" class="pull-right btn btn-xs btn-white">Mark</a>
+                      <i class="fa fa-clock-o"></i> 16.11.2015
+                    </div>
+                  </li>
+                  <li class="warning-element" id="task22">
+                    Simply dummy text of the printing and typesetting industry.
+                    <div class="agile-detail">
+                      <a href="#" class="pull-right btn btn-xs btn-white">Tag</a>
+                      <i class="fa fa-clock-o"></i> 12.10.2015
+                    </div>
+                  </li>
+                  <li class="success-element" id="task23">
+                    Many desktop publishing packages and web page editors now use Lorem Ipsum as their default.
+                    <div class="agile-detail">
+                      <a href="#" class="pull-right btn btn-xs btn-white">Mark</a>
+                      <i class="fa fa-clock-o"></i> 05.04.2015
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
 
   </div>
 </div>
 
+
+
+</div>
+
+
 <?php
 include('../../includes/footer.php');
 ?>
-
 </body>
 
 </html>
