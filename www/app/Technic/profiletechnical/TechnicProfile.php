fab6c62388324ef55430631387f87620d384f616
Mariano1234 <mariano22041993@gmail.com>
2024-09-07 20:06:14 -0300

    actualice

--- www/app/Technic/profiletechnical/TechnicProfile.php
+++ www/app/Technic/profiletechnical/TechnicProfile.php
@@ -5,60 +5,79 @@ define('PAGE', 'Technic Profile');
 include ('../../dbConnection.php');
 include ('../../Querys/querys.php');
 
-
-
-////////////////////////////////
+// Verificar que el usuario esté autenticado y tenga el rol correcto
 if ($_SESSION['is_login'] && $_SESSION['state_user'] == 'activo') {
-  if ( $_SESSION['user_idRol'] != 2) {
-    header("Location:../../includes/404/404.php");
-  }
-  $rEmail = $_SESSION['mail'];
-  $rolUser = $_SESSION['user_role'];
+    if ($_SESSION['user_idRol'] != 2) {
+        header("Location:../../includes/404/404.php");
+        exit;
+    }
+    $rEmail = $_SESSION['mail'];
+    $rolUser = $_SESSION['user_role'];
 } else {
-  echo "<script> location.href='../../login.php'; </script>";
+    echo "<script> location.href='../../login.php'; </script>";
+    exit;
 }
-////////////////////////////////
-// Genera un token CSRF y lo guarda en la sesión
+
+// Generar un token CSRF y guardarlo en la sesión
 if (!isset($_SESSION['token'])) {
     $_SESSION['token'] = bin2hex(random_bytes(32));
 }
 $token = $_SESSION['token'];
 
-$stmt = $conn->prepare(SQL_SELEC_USERS);
+// Preparar y ejecutar la consulta para obtener los datos del técnico
+$stmtr = $conn->prepare(SQL_SELEC_USERS);
 $technic_id = $_SESSION['user_id'];
+$stmtr->bind_param("i", $technic_id);
+$stmtr->execute();
+$result = $stmtr->get_result();
+$row = $result->fetch_assoc();
+
+if ($row) {
+    $surname = $row['surname'];
+    $name_user = $row['name_user'];
+    $mail = $row['mail'];
+    $phone_user = $row['phone_user'];
+} else {
+    $name_user = 'No disponible';
+    $surname = 'No disponible';
+    $mail = 'No disponible';
+    $phone_user = 'No disponible';
+}
+
+$stmtr->close();
+
+// Preparar y ejecutar la consulta para contar las órdenes con estado del técnico
+$stmt = $conn->prepare(SQL_COUNT_ORDERS_WITH_STATE_TECHNIC);
 $stmt->bind_param("i", $technic_id);
 $stmt->execute();
 $result = $stmt->get_result();
-$row = $result->fetch_assoc();
-    
-    $surname = $row['surname'];
-    
-    
+
+if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    $confirmadas = $row['confirmadas'];
+    $pendientes = $row['pendientes'];
+    $realizadas = $row['realizadas'];
+} else {
+    $confirmadas = $pendientes = $realizadas = 0;
+}
+$stmt->close();
 
 ?>
-<?php
-include ('../../includes/header.php'); ?>
+<?php include ('../../includes/header.php'); ?>
 
 <body>
-
     <div id="wrapper">
-
         <nav class="navbar-default navbar-static-side" role="navigation">
             <div class="sidebar-collapse">
-                <?php
-                include ('../../includes/menu.php'); ?>
-
+                <?php include ('../../includes/menu.php'); ?>
             </div>
         </nav>
-        
 
         <div id="page-wrapper" class="gray-bg">
-
             <div class="row border-bottom">
                 <nav class="navbar navbar-static-top" role="navigation" style="margin-bottom: 0">
                     <div class="navbar-header">
-                        <a class="navbar-minimalize minimalize-styl-2 btn btn-primary" href="#"><i
-                                class="fa fa-bars"></i> </a>
+                        <a class="navbar-minimalize minimalize-styl-2 btn btn-primary" href="#"><i class="fa fa-bars"></i> </a>
                     </div>
                     <ul class="nav navbar-top-links navbar-right">
                         <li>
@@ -67,106 +86,127 @@ include ('../../includes/header.php'); ?>
                             </a>
                         </li>
                     </ul>
-
                 </nav>
             </div>
+
             <div class="row wrapper border-bottom white-bg page-heading">
                 <div class="col-lg-10">
                     <h2>Perfil</h2>
-
                 </div>
-                <div class="col-lg-2">
-
-                </div>
+                <div class="col-lg-2"></div>
             </div>
+
             <div class="row m-b-lg m-t-lg">
                 <div class="col-md-6">
-
                     <div class="profile-image">
-                        <img src="../../img/logo.png" class="rounded-circle circle-border m-b-md" alt="profile">
+                        <img src="../../../img/team-3.jpg" class="rounded-circle circle-border m-b-md" alt="profile">
                     </div>
                     <div class="profile-info">
                         <div class="">
                             <div>
                                 <h2 class="no-margins">
-                                <?php
-                                 echo $name_user = $row['name_user'];  ?>
-                                  <?php echo $surname = $row['surname_user'];?>
+                                    <?php echo htmlspecialchars($name_user); ?>
+                                    <?php echo htmlspecialchars($surname); ?>
                                 </h2>
-                                <h4>Perfil del Tecnico</h4>
+                                <h4>Perfil del Técnico</h4>
                                 <small>
-                                técnico de mantenimiento informatico con 3 años de experiencia en el sector, 
-                                especializado en la gestión y ejecución de tareas de mantenimiento preventivo y correctivo.
+                                    Técnico de mantenimiento informático con 3 años de experiencia en el sector, 
+                                    especializado en la gestión y ejecución de tareas de mantenimiento preventivo y correctivo.
                                 </small>
                             </div>
                         </div>
                     </div>
                 </div>
+
                 <div class="col-md-3">
                     <table class="table small m-b-xs">
                         <tbody>
                         <tr>
                             <td>
-                                <strong>Email:</strong> <?php echo $mail = $row['mail'];?>
-                            </td>
-                           
-
-                        </tr>
-                        <tr>
-                             <td>
-                                <strong>Celular</strong> <?php echo $phone_user = $row['phone_user'];?>
+                                <strong>Email:</strong> <?php echo htmlspecialchars($mail); ?>
                             </td>
                         </tr>
                         <tr>
-                        <td>
-                                <strong>Cargo</strong> <?php if (isset($_SESSION['user_role'])) {
-                                              echo $_SESSION['user_role'];
-                                              
-                                            } ?>
+                            <td>
+                                <strong>Celular:</strong> <?php echo htmlspecialchars($phone_user); ?>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <strong>Cargo:</strong> <?php echo htmlspecialchars($rolUser); ?>
                             </td>
                         </tr>
                         </tbody>
                     </table>
                 </div>
+
                 <div class="text-center">
                     <button type="button" class="btn btn-primary" onclick="openEditModal()">
                         Cambiar contraseña
                     </button>
                 </div>
             </div>
-           
-            
 
-              
-
-
-
-            </div>
-            <div class="footer">
-                <div class="pull-right">
-
+            <div class="wrapper wrapper-content animated fadeInRight">
+                <div class="row">
+                    <div class="col-lg-4">
+                        <div class="widget style1 custom-bg-1">
+                            <div class="row">
+                                <div class="col-xs-4 text-center">
+                                    <i class="fa fa-check-square-o fa-5x"></i>
+                                </div>
+                                <div class="col-xs-8 text-right">
+                                    <span> Ordenes Confirmadas </span>
+                                    <h2 class="font-bold"><?php echo htmlspecialchars($confirmadas); ?></h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="widget style1 custom-bg-2">
+                            <div class="row">
+                                <div class="col-xs-4">
+                                    <i class="fa fa-check fa-5x"></i>
+                                </div>
+                                <div class="col-xs-8 text-right">
+                                    <span> Ordenes Finalizadas </span>
+                                    <h2 class="font-bold"><?php echo htmlspecialchars($realizadas); ?></h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="widget style1 custom-bg-3">
+                            <div class="row">
+                                <div class="col-xs-4">
+                                    <i class="fa fa-exclamation fa-5x"></i>
+                                </div>
+                                <div class="col-xs-8 text-right">
+                                    <span> Ordenes Pendientes </span>
+                                    <h2 class="font-bold"><?php echo htmlspecialchars($pendientes); ?></h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
+            </div>
+
+            <div class="footer">
+                <div class="pull-right"></div>
                 <div>
                     <strong>Copyright</strong> Telistema &copy; 2024
                 </div>
             </div>
-
         </div>
     </div>
 
-
-
-    <?php
-    include ('../../includes/footer.php');
-    ?>
-<div id="edit-form-container" style="display: none;"></div>
+    <?php include ('../../includes/footer.php'); ?>
+    <div id="edit-form-container" style="display: none;"></div>
     <!-- Sparkline -->
     <script src="js/plugins/sparkline/jquery.sparkline.min.js"></script>
 
     <script>
         $(document).ready(function () {
-
-
             $("#sparkline1").sparkline([34, 43, 43, 35, 44, 32, 44, 48], {
                 type: 'line',
                 width: '100%',
@@ -177,23 +217,21 @@ include ('../../includes/header.php'); ?>
         });
 
         function openEditModal() {
-                // Realiza una solicitud AJAX para obtener el formulario de edición
-                $.ajax({
-                    url: "modalpass.php?token=<?php echo $token; ?>", // Ruta al archivo de edición de usuario
-                    type: "GET",
-                    success: function (response) {
-                        // Muestra el formulario de edición en el contenedor
-                        $("#edit-form-container").html(response).slideDown();
-                        // Abre el modal
-                        $("#myModal6").modal("show");
-                    },
-                    error: function () {
-                        alert("Error al cargar el formulario de edición.");
-                    }
-                });
-            }
+            // Realiza una solicitud AJAX para obtener el formulario de edición
+            $.ajax({
+                url: "modalpass.php?token=<?php echo $token; ?>", // Ruta al archivo de edición de usuario
+                type: "GET",
+                success: function (response) {
+                    // Muestra el formulario de edición en el contenedor
+                    $("#edit-form-container").html(response).slideDown();
+                    // Abre el modal
+                    $("#myModal6").modal("show");
+                },
+                error: function () {
+                    alert("Error al cargar el formulario de edición.");
+                }
+            });
+        }
     </script>
-
 </body>
-
 </html>
