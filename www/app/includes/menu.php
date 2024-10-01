9702f37a3dac2a8a7b4e42ce49be6d0e9529ae76
christian.pereyra_ <new_chriss@hotmail.com.ar>
2024-10-01 16:21:44 -0300

    menú-organized

--- www/app/includes/menu.php
+++ www/app/includes/menu.php
@@ -9,192 +9,101 @@ if ($_SESSION['is_login'] && $_SESSION['state_user'] == 'activo') {
 } else {
     echo "<script> location.href='../login.php'; </script>";
 }
-
-// Determina la URL dashboard según el rol
-// $dashboard_url = '';
-// if ($rolUser == 'admin') {
-//     $dashboard_url = '../../Admin/dashboard/dashboard.php';
-// } elseif ($rolUser == 'technic') {
-//     $dashboard_url = '/var/www/html/app/Technic/dashboard/dashboard.php';
-// }
-
 ?>
+
 <ul class="nav metismenu" id="side-menu">
     <li class="nav-header">
         <div class="dropdown profile-element">
             <a data-toggle="dropdown" class="dropdown-toggle" href="#">
-                <span class="clear"> <span class="block m-t-xs"> <strong
-                            class="font-bold"><?php echo ucfirst($_SESSION['user_name']) ?></strong>
-                    </span> <span class="text-muted text-xs block"><?php echo ucfirst($_SESSION['user_role']) ?><b
-                            class="caret"></b></span>
-                </span> 
-                
-        <div class="logo-element">
-            TL+
-        </div>
+                <span class="clear"> <span class="block m-t-xs"> <strong class="font-bold"><?php echo ucfirst($_SESSION['user_name']) ?></strong>
+                    </span> <span class="text-muted text-xs block"><?php echo ucfirst($_SESSION['user_role']) ?><b class="caret"></b></span>
+                </span>
+
+                <div class="logo-element">
+                    TL+
+                </div>
     </li>
 
     <?php if ($rolUser == 1) { ?>
 
         <li class=" <?php if (PAGE == 'dashboard admin') {
-            echo 'active';
-        } ?>">
+                        echo 'active';
+                    } ?>">
             <a href="../../Admin/dashboard/dashboard.php">
                 <i class="bi bi-house"></i> <span class="nav-label">Inicio</span>
             </a>
         </li>
         <li class=" <?php if (PAGE == 'Ordenes Admin') {
-            echo 'active';
-        } ?>">
+                        echo 'active';
+                    } ?>">
             <a href="#"><i class="bi bi-receipt"></i><span class="nav-label">Ordenes de Trabajo</span><span class="fa arrow"></span></a>
-                <ul class="nav nav-second-level collapse">
-                    <li><a href="../crudorders/order.php">Ordenes Asignadas</a></li>
-                    <li><a href="../crudorders/reportsOrders.php">Reportes </a></li>
-                </ul>
+            <ul class="nav nav-second-level collapse">
+                <li><a href="../crudorders/order.php">Ordenes Asignadas</a></li>
+                <li><a href="../crudorders/reportsOrders.php">Reportes </a></li>
+            </ul>
         </li>
         <li class=" <?php if (PAGE == 'Compras') {
-
-            echo 'active';
-        } ?>">
-            <a href="../crudbuys/buys.php">
-                <i class="fa fa-shopping-cart"></i><span class="nav-label">
-                    Compras</span>
-            </a>
+                        echo 'active';
+                    } ?>">
+            <a href="#"><i class="bi bi-people-fill"></i><span class="nav-label">Miembros</span><span class="fa arrow"></span></a>
+            <ul class="nav nav-second-level collapse">
+                <li><a href="../crudusers/users.php">Usuarios</a></li>
+                <li><a href="../crudclients/clients.php">Clientes </a></li>
+                <li><a href="../crudsuppliers/suppliers.php">Proveedores</a></li>
+            </ul>
         </li>
-        <li class=" <?php if (PAGE == 'Proveedores') {
-
-            echo 'active';
-        } ?>">
-            <a href="../crudsuppliers/suppliers.php">
-                <i class="bi bi-database"></i><span class="nav-label">Proveedores</span>
-
-            </a>
+        <li class=" <?php if (PAGE == 'Compras') {
+                        echo 'active';
+                    } ?>">
+            <a href="#"><i class="fa fa-shopping-cart"></i><span class="nav-label">Tienda</span><span class="fa arrow"></span></a>
+            <ul class="nav nav-second-level collapse">
+                <li><a href="../crudbuys/buys.php">Compras</a></li>
+                <li><a href="../crudmaterials/materials.php">Productos </a></li>
+            </ul>
         </li>
-        <li class=" <?php if (PAGE == 'Materiales') {
-            echo 'active';
-        } ?>">
-
-            <a href="../crudmaterials/materials.php">
-                <i class="bi bi-database"></i><span class="nav-label">Productos</span>
-
-            </a>
-        </li>
-     
-        <li class=" <?php if (PAGE == 'Clientes') {
-            echo 'active';
-        } ?>">
-            <a href="../crudclients/clients.php">
-                <i class="bi bi-people-fill"></i><span class="nav-label">
-                    Clientes</span>
-            </a>
-        </li>
-        <li class=" <?php if (PAGE == 'usuarios') {
-            echo 'active';
-        } ?>">
-            <a href="../crudusers/users.php">
-                <i class="bi bi-people-fill"></i><span class="nav-label">
-                    Usuarios</span>
-            </a>
-        </li>
-        <!-- <li class=" <?php if (PAGE == 'sellreport') {
-            echo 'active';
-        } ?>">
-            <a href="../Admin/soldproductreport.php">
-                <i class="bi bi-bootstrap-reboot"></i><span class="nav-label">
-                    Reportes de Materiales</span>
-            </a>
-        </li>
-        <li class=" <?php if (PAGE == 'workreport') {
-            echo 'active';
-        } ?>">
-            <a href="../Admin/workreport.php">
-                <i class="bi bi-bootstrap-reboot"></i><span class="nav-label">
-                    Reportes de Ordenes</span>
-            </a>
-        </li> -->
 
         <li class=" <?php if (PAGE == 'Actualización de Email') {
-            echo 'active';
-        } ?>">
-            <a href="../configsmtp/configEmailContact.php">
-                <i class="bi bi-envelope-at"></i><span class="nav-label">
-                    Email de Contacto</span>
-            </a>
-            <!-- </li>
-        <li class=" <?php if (PAGE == 'changepass') {
-            echo 'active';
-        } ?>
-        ">
-            <a href="../Admin/changepass.php">
-                <i class="bi bi-key"></i><span class="nav-label">
-                    Cambiar Clave</span>
-            </a>
-        </li> -->
-        
-        <li class=" <?php if (PAGE == 'Admin Profile') {
-            echo 'active';
-        } ?>">
-            <a href="../../Admin/profileAdmin/AdminProfile.php">
-                <i class="bi bi-person-circle"></i></i><span class="nav-label">
-                    Perfil <span class="sr-only">(current)</span></span>
-            </a>
+                        echo 'active';
+                    } ?>">
+            <a href="#"><i class="fa fa-gears"></i><span class="nav-label">Configuraciones</span><span class="fa arrow"></span></a>
+            <ul class="nav nav-second-level collapse">
+                <li><a href="../configsmtp/configEmailContact.php">Email de Contacto</a></li>
+                <li><a href="../../Admin/profileAdmin/AdminProfile.php">Perfil </a></li>
+            </ul>
         </li>
 
-        <?php } ?>
+    <?php } ?>
 
-        <!-- TECNICO -->
-        <?php if ($rolUser == 2) { ?>
+    <!-- TECNICO -->
+    <?php if ($rolUser == 2) { ?>
 
         <li class=" <?php if (PAGE == 'dashboard tecnico') {
-            echo 'active';
-        } ?>">
+                        echo 'active';
+                    } ?>">
             <a href="../../Technic/dashboard/dashboard.php">
                 <i class="bi bi-house"></i> <span class="nav-label">Inicio</span>
             </a>
         </li>
 
         <li class=" <?php if (PAGE == 'Technic Profile') {
-            echo 'active';
-        } ?>">
+                        echo 'active';
+                    } ?>">
             <a href="../../Technic/profiletechnical/TechnicProfile.php">
                 <i class="bi bi-person-circle"></i></i><span class="nav-label">
                     Perfil <span class="sr-only">(current)</span></span>
             </a>
         </li>
-        <!-- <li class=" <?php if (PAGE == 'SubmitTechnic') {
-            echo 'active';
-        } ?>">
-            <a href="../Technic/SubmitTechnic.php">
-                <i class="bi bi-person-wheelchair"></i><span class="nav-label">
-                    Enviar Pedido</span>
-            </a>
-        </li> -->
+
         <li class=" <?php if (PAGE == 'Ordenes') {
-            echo 'active';
-        } ?>">
+                        echo 'active';
+                    } ?>">
             <a href="#"><i class="bi bi-receipt"></i><span class="nav-label">Ordenes Asignadas</span><span class="fa arrow"></span></a>
-                <ul class="nav nav-second-level collapse">
-                    <li><a href="../../Technic/orders/ordersTechnic.php">Ordenes Asignadas</a></li>
-                    <li><a href="../../Technic/orders/reportsOrders.php">Reportes </a></li>
-                    <li><a href="../../Technic/orders/historyOrders.php">Historial </a></li>
-                </ul>
+            <ul class="nav nav-second-level collapse">
+                <li><a href="../../Technic/orders/ordersTechnic.php">Ordenes Asignadas</a></li>
+                <li><a href="../../Technic/orders/reportsOrders.php">Reportes </a></li>
+                <li><a href="../../Technic/orders/historyOrders.php">Historial </a></li>
+            </ul>
         </li>
-        <!-- <li class=" <?php if (PAGE == 'CheckStatus') {
-            echo 'active';
-        } ?>">
-            <a href="../Technic/CheckStatus.php">
-                <i class="bi bi-list-nested"></i></i><span class="nav-label">
-                    Estado Servicio</span>
-            </a>
-        </li> -->
-        <!-- <li class=" <?php if (PAGE == 'Requesterchangepass') {
-            echo 'active';
-        } ?>">
-            <a href="../Technic/TechnicChangePass.php">
-                <i class="bi bi-key"></i><span class="nav-label">
-                    Cambiar Clave</span>
-            </a>
-        </li> -->
 
     <?php } ?>
 
