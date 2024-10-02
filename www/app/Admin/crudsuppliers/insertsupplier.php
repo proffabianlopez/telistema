be345ed8d7ee0072c74b1d5afb9b9eafdcdaacb3
yazpenayo <yazmin.penayo@hotmail.com>
2024-10-02 13:07:02 -0300

    Proveedor validaciones

--- www/app/Admin/crudsuppliers/insertsupplier.php
+++ www/app/Admin/crudsuppliers/insertsupplier.php
@@ -47,7 +47,7 @@ include ('../../Querys/querys.php');
                   </div>
                   <div class="form-group">
                     <label for="phone">Telefono <span class="text-danger" >*</span></label>
-                    <input type="number" class="form-control validate-field vphone" id="phone" name="phone">
+                    <input type="tel" class="form-control validate-field" id="phone" name="phone">
                   </div>
                 </div>
                 <!-- Segunda columna -->
@@ -85,7 +85,22 @@ include ('../../Querys/querys.php');
     let email = '';
 </script>
 <script src="../../js/main.js"></script>
+<script>
+  const input = document.querySelector("#phone");
+  const iti = window.intlTelInput(input, {
+    separateDialCode: true,
+    preferredCountries: ["ar", "us", "br", "mx"],
+    utilsScript: "../../js/utils.js"
+  });
 
+  document.getElementById('change-insertsupplier-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const fullNumber = iti.getNumber();
+    console.log("Número completo con código de área:", fullNumber);
+    document.getElementById('phone').value = fullNumber;
+    this.submit();
+  });
+  </script>
 </body>
 
 </html>>
