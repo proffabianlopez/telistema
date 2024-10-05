1e95595e7de91b7e998ff42ca5d459e82505a587
MV <mev_tec@yahoo.com.ar>
2024-10-05 18:49:31 -0300

    botones responsivos

--- www/app/Admin/profileAdmin/AdminProfile.php
+++ www/app/Admin/profileAdmin/AdminProfile.php
@@ -3,7 +3,7 @@ session_start();
 
 // Verificar si el usuario está logueado y activo
 if (!isset($_SESSION['is_login']) || $_SESSION['state_user'] != 'activo') {
-    echo "<script>location.href='../../login.php';</script>";
+    echo "<script> location.href='../../login.php'; </script>";
     exit;
 }
 
@@ -33,6 +33,7 @@ $stmt->execute();
 
 // Obtener resultados de la consulta
 $result = $stmt->get_result();
+// Verificar si hay resultados
 if ($result->num_rows > 0) {
     // Obtener la fila como un array asociativo
     $row = $result->fetch_assoc();
@@ -40,29 +41,26 @@ if ($result->num_rows > 0) {
     echo '<div class="alert alert-warning col-sm-6 ml-5 mt-2" role="alert">No se encontró ningún usuario con el ID proporcionado.</div>';
     exit;
 }
+
 ?>
 
 <body>
     <div id="wrapper">
-        <!-- Barra lateral -->
         <nav class="navbar-default navbar-static-side" role="navigation">
             <div class="sidebar-collapse">
                 <?php include('../../includes/menu.php') ?>
             </div>
         </nav>
 
-        <!-- Contenido principal -->
         <div id="page-wrapper" class="gray-bg">
-            <!-- Barra de navegación -->
             <div class="row border-bottom">
                 <nav class="navbar navbar-static-top" role="navigation" style="margin-bottom: 0">
                     <div class="navbar-header">
-                        <a class="navbar-minimalize minimalize-styl-2 btn btn-primary" href="#"><i class="fa fa-bars"></i></a>
+                        <a class="navbar-minimalize minimalize-styl-2 btn btn-primary" href="#"><i class="fa fa-bars"></i> </a>
                     </div>
                 </nav>
             </div>
 
-            <!-- Encabezado de página -->
             <div class="row wrapper border-bottom white-bg page-heading">
                 <div class="col-lg-10">
                     <h2>Mi Perfil</h2>
@@ -70,91 +68,114 @@ if ($result->num_rows > 0) {
                 <div class="col-lg-2"></div>
             </div>
 
-            <!-- Contenido del perfil -->
-            <div class="wrapper wrapper-content animated fadeInRight">
-                <div class="row m-b-lg m-t-lg ibox-content">
-                    <!-- Imagen de perfil -->
-                    <div class="col-md-6">
-                        <div class="profile-image">
-                            <img src="../../../img/team-3.jpg" class="rounded-circle circle-border m-b-md" alt="profile">
-                        </div>
+            <div class="row wrapper border-bottom white-bg m-t-sm">
+                <div class="row m-t-md m-b-md">
+                    <div class="col-md-6 m-b">
+                        <a id="edit-<?php echo ($row["id_user"] . '-' . $token); ?>" data-crud="adminAvatar" class="modaledit-btn">
+                            <div class="profile-image m-t-sm">
+                                <img src="<?php echo htmlspecialchars($row['avatar_user']); ?>"  alt="Avatar de <?php echo htmlspecialchars($row['name_user']);?>" class="img-circle img-thumbnail">
+                            </div>
+                        </a>
+
                         <div class="profile-info">
-                            <h2 class="no-margins">
-                                <?php echo htmlspecialchars($row['name_user']); ?>
-                                <?php echo htmlspecialchars($row['surname_user']); ?>
-                            </h2>
-                            <h4>Perfil del Administrador</h4>
+                            <div class="row">
+                                <div class="col">
+                                    <h2>
+                                        <?php echo htmlspecialchars($row['name_user']); ?>
+                                        <?php echo htmlspecialchars($row['surname_user']); ?>
+                                    </h2>
+                                    <h4>Perfil del Administrador</h4>
+                                </div>
+                            </div>
                         </div>
                     </div>
 
-                    <!-- Información del perfil -->
-                    <div class="col-md-3">
+                    <div class="col-md-3 m-b">
                         <table class="table small m-b-xs">
                             <tbody>
-                                <tr>
-                                    <td><strong>Email:</strong> <?php echo htmlspecialchars($row['mail']); ?></td>
-                                </tr>
-                                <tr>
-                                    <td><strong>Celular:</strong> <?php echo htmlspecialchars($row['phone_user']); ?></td>
-                                </tr>
-                                <tr>
-                                    <td><strong>Cargo:</strong>
-                                        <?php
-                                        switch ($row['id_rol']) {
-                                            case 1:
-                                                echo 'Administrador';
-                                                break;
-                                            case 2:
-                                                echo 'Técnico';
-                                                break;
-                                            default:
-                                                echo 'Desconocido';
-                                                break;
-                                        }
-                                        ?>
-                                    </td>
-                                </tr>
+                            <tr>
+                                <td>
+                                    <strong>Email:</strong> <?php echo htmlspecialchars($row['mail']); ?>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <strong>Celular:</strong> <?php echo htmlspecialchars($row['phone_user']); ?>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <strong>Cargo:</strong>
+                                    <?php
+                                    switch ($row['id_rol']) {
+                                        case 1:
+                                            echo 'Administrador';
+                                            break;
+                                        case 2:
+                                            echo 'Técnico';
+                                            break;
+                                        default:
+                                            echo 'Desconocido';
+                                            break;
+                                    }
+                                    ?>
+                                </td>
+                            </tr>
                             </tbody>
                         </table>
                     </div>
 
-                    <!-- Botones de acción -->
-                    <div class="col-md-3 d-flex flex-column align-items-stretch" role="group">
-                        <div>   
-                            <button id="edit-<?php echo ($row["id_user"] . '-' . $token); ?>" data-crud="admin" class="btn btn-primary modaledit-btn m-b-xs m-md-b-sm ">Modificar perfil</button>
-                        </div>
-                        <div>            
-                             <button id="edit-<?php echo ($row["id_user"] . '-' . $token); ?>" data-crud="adminPassword" class="btn btn-primary modaledit-btn">Cambiar Contraseña</button>
-                             </div>
+                    <div class="col-md-3 m-b-sm m-t-sm"  role="group">
+                        <button id="edit-<?php echo ($row["id_user"] . '-' . $token); ?>" data-crud="admin" class="btn btn-primary modaledit-btn m-b-sm">
+                            Modificar mi perfil
+                        </button>
+                        <br class="mb-lg-2 mb-sm-1">
+                        <button id="edit-<?php echo ($row["id_user"] . '-' . $token); ?>" data-crud="adminPassword" class="btn btn-primary modaledit-btn">
+                            Cambiar Contraseña
+                        </button>
                     </div>
                 </div>
             </div>
 
-            <!-- Pie de página -->
+            </div>
             <div class="footer">
                 <div class="pull-right"></div>
-                <div><strong>Copyright</strong> Telistema &copy; 2024</div>
+                <div>
+                    <strong>Copyright</strong> Telistema &copy; 2024
+                </div>
             </div>
         </div>
     </div>
-
-    <!-- Contenedores ocultos para formularios de edición -->
     <div id="editpassword-form-container" style="display: none;"></div>
     <div id="edit-form-container" style="display: none;"></div>
-
-    <!-- Scripts -->
     <script>
-        let token = "<?php echo $token; ?>";
-        let email = "<?php echo $row['mail']; ?>";
+        let token = "<?php echo $token; ?>"
+        let email = "<?php echo $row['mail']; ?>"
     </script>
-
     <?php include('../../includes/footer.php'); ?>
-
     <script>
         $(document).ready(function () {
             $('.footable').footable();
             $('.footable2').footable();
         });
+
+        // Cambiar contraseña
+       /* function openEditModal() {
+            // Realiza una solicitud AJAX para obtener el formulario de edición
+            $.ajax({
+                url: "../crudusers/modalpass.php?token=<?php echo $token; ?>", // Ruta al archivo de edición de usuario
+                type: "GET",
+                success: function (response) {
+                    // Muestra el formulario de edición en el contenedor
+                    $("#editpassword-form-container").html(response).slideDown();
+                    // Abre el modal
+                    $("#myModal6").modal("show");
+                },
+                error: function () {
+                    alert("Error al cargar el formulario de edición.");
+                }
+            });
+        }*/
     </script>
 </body>
 </html>
