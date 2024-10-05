c57ac2738ce4335b537f5f9cc2c5b5ac95e5cc95
Mariano1234 <mariano22041993@gmail.com>
2024-10-05 19:00:01 -0300

    alimine boton cerrar sesion

--- www/app/Technic/profiletechnical/TechnicProfile.php
+++ www/app/Technic/profiletechnical/TechnicProfile.php
@@ -62,13 +62,7 @@ $stmtr->close();
                     <div class="navbar-header">
                         <a class="navbar-minimalize minimalize-styl-2 btn btn-primary" href="#"><i class="fa fa-bars"></i> </a>
                     </div>
-                    <ul class="nav navbar-top-links navbar-right">
-                        <li>
-                            <a href="../logout.php">
-                                <i class="fa fa-sign-out"></i> Cerrar Sección
-                            </a>
-                        </li>
-                    </ul>
+                  
                 </nav>
             </div>
 
